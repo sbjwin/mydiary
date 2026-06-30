8cdef3a33cc6946331ade6cd76f73d94d33a5bbb
sbjwin <sbjwin4271@gmail.com>
2026-07-01 01:02:11 +0900

    adb 실행 오류 완전히 해결

---
+++ android/app/.cxx/Debug/5z622i38/prefab/x86/prefab/lib/i686-linux-android/cmake/fbjni/fbjniConfig.cmake
@@ -0,0 +1,9 @@
+if(NOT TARGET fbjni::fbjni)
+add_library(fbjni::fbjni SHARED IMPORTED)
+set_target_properties(fbjni::fbjni PROPERTIES
+    IMPORTED_LOCATION "C:/Users/BaekjinSung/.gradle/caches/9.3.1/transforms/6f1f7af63467440b9fe6f23d6ac7fd9b/workspace/transformed/fbjni-0.7.0/prefab/modules/fbjni/libs/android.x86/libfbjni.so"
+    INTERFACE_INCLUDE_DIRECTORIES "C:/Users/BaekjinSung/.gradle/caches/9.3.1/transforms/6f1f7af63467440b9fe6f23d6ac7fd9b/workspace/transformed/fbjni-0.7.0/prefab/modules/fbjni/include"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
