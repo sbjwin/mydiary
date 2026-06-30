b6815521e4df712b9f4fdb3e9fc4ed7f103d3ba3
sbjwin <sbjwin4271@gmail.com>
2026-06-30 23:58:14 +0900

    expo로 테스트가 가능하게 설정하기

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  preset: '@react-native/jest-preset',
+  preset: 'jest-expo',
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@react-native|@react-navigation|react-navigation|react-native-calendars|react-native-vector-icons|@react-native-async-storage/async-storage|expo|@expo(nent)?/.*)',
+  ],
 };
