e3f119f61f4b4f3662b8746330a8bbf2a02cff42
sbjwin <sbjwin4271@gmail.com>
2026-07-12 17:17:52 +0900

    test: Jest 테스트 환경 오류 수정 및 Mock 설정 추가

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: 'jest-expo',
+  setupFiles: ['<rootDir>/jest.setup.js'],
   transformIgnorePatterns: [
     'node_modules/(?!(jest-)?react-native|@react-native|@react-navigation|react-navigation|react-native-calendars|react-native-vector-icons|@react-native-async-storage/async-storage|expo|@expo(nent)?/.*)',
   ],
---
+++ jest.setup.js
@@ -0,0 +1,24 @@
+import 'react-native-gesture-handler/jestSetup';
+
+jest.useFakeTimers();
+
+jest.mock('@react-native-async-storage/async-storage', () =>
+  require('@react-native-async-storage/async-storage/jest/async-storage-mock')
+);
+
+jest.mock('@react-native-google-signin/google-signin', () => ({
+  GoogleSignin: {
+    configure: jest.fn(),
+    hasPlayServices: jest.fn(() => Promise.resolve(true)),
+    signIn: jest.fn(() => Promise.resolve({ user: { id: 'test-user', email: 'test@example.com' } })),
+    signOut: jest.fn(() => Promise.resolve()),
+    isSignedIn: jest.fn(() => Promise.resolve(false)),
+    getTokens: jest.fn(() => Promise.resolve({ accessToken: 'test-token' })),
+  },
+  statusCodes: {
+    SIGN_IN_CANCELLED: 'SIGN_IN_CANCELLED',
+    IN_PROGRESS: 'IN_PROGRESS',
+    PLAY_SERVICES_NOT_AVAILABLE: 'PLAY_SERVICES_NOT_AVAILABLE',
+  },
+}));
+
